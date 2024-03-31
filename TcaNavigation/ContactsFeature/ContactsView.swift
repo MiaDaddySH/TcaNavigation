e8c0966a2ff590989d0853e8e87dc6eaf174256f
Yuangang Sheng <yuangang.sheng@intive.com>
2024-03-31 12:47:29 +0200

    Add contacts view

---
+++ TcaNavigation/ContactsFeature/ContactsView.swift
@@ -0,0 +1,50 @@
+//
+//  ContactsView.swift
+//  TcaNavigation
+//
+//  Created by Yuangang Sheng on 31.03.24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ContactsView: View {
+  let store: StoreOf<ContactsFeature>
+  
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach(store.contacts) { contact in
+          Text(contact.name)
+        }
+      }
+      .navigationTitle("Contacts")
+      .toolbar {
+        ToolbarItem {
+          Button {
+            store.send(.addButtonTapped)
+          } label: {
+            Image(systemName: "plus")
+          }
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  ContactsView(
+    store: Store(
+      initialState: ContactsFeature.State(
+        contacts: [
+          Contact(id: UUID(), name: "Blob"),
+          Contact(id: UUID(), name: "Blob Jr"),
+          Contact(id: UUID(), name: "Blob Sr"),
+        ]
+      )
+    ) {
+      ContactsFeature()
+    }
+  )
+}
+
