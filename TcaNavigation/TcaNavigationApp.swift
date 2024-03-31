a86473bab623f3b49eb953394a991e3866ae4c42
Yuangang Sheng <yuangang.sheng@intive.com>
2024-03-31 11:19:21 +0200

    add project

---
+++ TcaNavigation/TcaNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcaNavigationApp.swift
+//  TcaNavigation
+//
+//  Created by Yuangang Sheng on 31.03.24.
+//
+
+import SwiftUI
+
+@main
+struct TcaNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
