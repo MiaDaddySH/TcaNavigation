c7e7b4fede2674fcc37d2a324aac2335f444ede0
Yuangang Sheng <yuangang.sheng@intive.com>
2024-04-01 17:24:10 +0200

    Added unit test for add button

---
+++ TcaNavigationTests/ContactsFeatureTest.swift
@@ -0,0 +1,48 @@
+//
+//  ContactsFeatureTest.swift
+//  TcaNavigationTests
+//
+//  Created by Yuangang Sheng on 01.04.24.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import TcaNavigation
+
+@MainActor
+final class ContactsFeatureTests: XCTestCase {
+  func testAddFlow() async {
+    let store = TestStore(initialState: ContactsFeature.State()) {
+      ContactsFeature()
+    } withDependencies: {
+      $0.uuid = .incrementing
+    }
+
+    await store.send(.addButtonTapped) {
+      $0.destination = .addContact(
+        AddContactFeature.State(
+          contact: Contact(id: UUID(0), name: "")
+        )
+      )
+    }
+
+    await store.send(\.destination.addContact.setName, "Blob Jr.") {
+      $0.destination?.addContact?.contact.name = "Blob Jr."
+    }
+
+    await store.send(\.destination.addContact.saveButtonTapped)
+    await store.receive(
+      \.destination.addContact.delegate.saveContact,
+      Contact(id: UUID(0), name: "Blob Jr.")
+    ) {
+      $0.contacts = [
+        Contact(id: UUID(0), name: "Blob Jr.")
+      ]
+    }
+    
+    await store.receive(\.destination.dismiss) {
+      $0.destination = nil
+    }
+  }
+}
