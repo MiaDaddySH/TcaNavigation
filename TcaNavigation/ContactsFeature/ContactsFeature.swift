2e680fa14f521bf45c0ab260fb2e8809a11fbfbe
Yuangang Sheng <yuangang.sheng@intive.com>
2024-04-01 16:24:30 +0200

    Add alert view

--- TcaNavigation/ContactsFeature/ContactsFeature.swift
+++ TcaNavigation/ContactsFeature/ContactsFeature.swift
@@ -19,11 +19,18 @@ struct ContactsFeature {
   struct State: Equatable {
     var contacts: IdentifiedArrayOf<Contact> = []
     @Presents var addContact: AddContactFeature.State?
+    @Presents var alert: AlertState<Action.Alert>?
   }
 
   enum Action {
     case addButtonTapped
     case addContact(PresentationAction<AddContactFeature.Action>)
+    case alert(PresentationAction<Alert>)
+    case deleteButtonTapped(id: Contact.ID)
+
+    enum Alert: Equatable {
+      case confirmDeletion(id: Contact.ID)
+    }
   }
 
   var body: some ReducerOf<Self> {
@@ -32,17 +39,35 @@ struct ContactsFeature {
       case .addButtonTapped:
         state.addContact = AddContactFeature.State(contact: .init(id: UUID(), name: ""))
         return .none
-        
+
       case let .addContact(.presented(.delegate(.saveContact(contact)))):
         state.contacts.append(contact)
         return .none
-        
+
       case .addContact:
+        return .none
+
+      case let .alert(.presented(.confirmDeletion(id: id))):
+        state.contacts.remove(id: id)
+        return .none
+
+      case .alert:
+        return .none
+
+      case let .deleteButtonTapped(id: id):
+        state.alert = AlertState {
+          TextState("Are you sure?")
+        } actions: {
+          ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+            TextState("Delete")
+          }
+        }
         return .none
       }
     }
     .ifLet(\.$addContact, action: \.addContact) {
       AddContactFeature()
     }
+    .ifLet(\.$alert, action: \.alert)
   }
 }
